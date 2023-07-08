70e26065bf582325db434b75daeccb55e185b5f5
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-07-08 22:56:37 +0530

    content updated

--- storepal-frontend/src/pages/Login.jsx
+++ storepal-frontend/src/pages/Login.jsx
@@ -10,39 +10,41 @@ function Login() {
   return (
     <>
       <Container>
-        <Form>
-          <h1>Login to your account</h1>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Button variant="primary" type="submit">
-              Login
-            </Button>
-          </Form.Group>
-          <p>
-            Don't have an account? <Link to="/signup">Create account</Link>
-          </p>
-        </Form>
         <Row>
+          <Col md={6} className="loginContainer">
+            <Form>
+              <h1>Login to your account</h1>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Button variant="primary" type="submit">
+                  Login
+                </Button>
+              </Form.Group>
+              <p>
+                Don't have an account? <Link to="/signup">Create account</Link>
+              </p>
+            </Form>
+          </Col>
           <Col xs={6} md={4} className="image_login"></Col>
         </Row>
       </Container>
--- storepal-frontend/src/pages/Signup.jsx
+++ storepal-frontend/src/pages/Signup.jsx
@@ -9,40 +9,42 @@ function Signup() {
   return (
     <>
       <Container>
-        <Form>
-          <h1>Create new account</h1>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Button variant="primary" type="submit">
-              Signup
-            </Button>
-          </Form.Group>
-          <p>
-            Don't have an account? <Link to="/signup">Create account</Link>{" "}
-          </p>
-        </Form>
         <Row>
-          <Col xs={6} md={4} className="image_signup"></Col>
+          <Col md={6} className="signupContainer">
+            <Form>
+              <h1>Create new account</h1>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Button variant="primary" type="submit">
+                  Signup
+                </Button>
+              </Form.Group>
+              <p>
+                Don't have an account? <Link to="/signup">Create account</Link>{" "}
+              </p>
+            </Form>
+          </Col>
+          <Col md={6} className="image_signup"></Col>
         </Row>
       </Container>
     </>
